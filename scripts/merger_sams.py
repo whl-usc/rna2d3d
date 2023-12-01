dfe318fb57eb71c27835e37c63640e1f93da9da5
Wilson <67292323+whl-usc@users.noreply.github.com>
2023-12-01 15:43:04 -0800

    Fixing merger_sams.py again for file comprehension issue.

--- scripts/merger_sams.py
+++ scripts/merger_sams.py
@@ -17,50 +17,59 @@ import sys
 def timenow():
     return str(datetime.now())[:-7]
 
-def process_sam_file(sam_file, label, sam_type, out_sam):
-    for line in sam_file:
-        if line.startswith("@"):
-            out_sam.write(line)
+# Usage instructions.
+if len(sys.argv) < 5:
+    print("Usage: python merger.py  sam_1  sam_2  label  samtype  outsam")
+    print("sam_1:   sam file from first round mapping")
+    print("sam_2:  sam file from second round mapping")
+    print("label:       label that will be added after readID, or 'none'")
+    print("samtype:     cont, gap1, gapm, homo, trans")
+    print("outsam:     output file name")
+    sys.exit()
+
+# Processing the sam file
+print(timenow() + " Started merger.py ...")
+
+sam1 = open(sys.argv[1],"r")
+sam2 = open(sys.argv[2],"r")
+label = sys.argv[3]
+samtype = sys.argv[4]
+outsam = open(sys.argv[5],"w")
+
+#2. Processing the sam file
+################################################################################
+print(timenow()+" Started merger.py ...")
+header='';
+if label != 'none':
+    for line in sam1:
+        if line[0]=="@": outsam.write(line)
         else:
             align = line.rstrip('\n').split()
-            if len(align) > 0:
-                if sam_type == "trans":
-                    outsam.write(align[0]+'-'+label+'\t'+'\t'.join(align[1:])+'\n')
-                else:
-                    outsam.write(align[0]+'-'+label+'\t'+'\t'.join(align[1:19])+'\n')
-# Usage instructions:
-def main():
-    if len(sys.argv) < 6:
-        print("Usage: python merger_sams.py first_sam second_sam label sam_type outputfile")
-        print("first_sam:   sam file from the first round mapping")
-        print("second_sam:  sam file from the second round mapping")
-        print("label:       label that will be added after readID, or 'none'")
-        print("sam_type:    cont, gap1, gapm, homo, trans")
-        print("outputfile:  the name of the output file")
-        sys.exit()
-
-    sam1 = open(sys.argv[1], "r")
-    sam2 = open(sys.argv[2], "r")
-    label = sys.argv[3]
-    sam_type = sys.argv[4]
-    out_sam = open(sys.argv[5], "w")
-
-    print(timenow() + " Started merger_sams.py ...")
-
-    for line in sam1:
-        if line.startswith("@"):
-            out_sam.write(line)
-    process_sam_file(sam1, label, sam_type, out_sam)
-
+            if len(align)>0:
+                if samtype=="trans":    outsam.write(align[0]+'-'+label+'\t'+'\t'.join(align[1:])+'\n')
+                else:                   outsam.write(align[0]+'-'+label+'\t'+'\t'.join(align[1:19])+'\n')
     for line in sam2:
-        if line.startswith("@"):
-            out_sam.write(line)
-    process_sam_file(sam2, label, sam_type, out_sam)
+        if line[0]!="@":
+            align = line.rstrip('\n').split()
+            if len(align)>0:
+                if samtype=="trans":    outsam.write(align[0]+'-'+label+'\t'+'\t'.join(align[1:])+'\n')
+                else:                   outsam.write(align[0]+'-'+label+'\t'+'\t'.join(align[1:19])+'\n')
 
-    sam1.close()
-    sam2.close()
-    out_sam.close()
+else:
+    for line in sam1:
+        if line[0]=="@": outsam.write(line)
+        else:
+            align = line.rstrip('\n').split()
+            if len(align)>0:
+                if samtype=="trans":    outsam.write(line)
+                else:                   outsam.write('\t'.join(align[0:19])+'\n')
+    for line in sam2:
+        if line[0]!="@":
+            align = line.rstrip('\n').split()
+            if len(align)>0:
+                if samtype=="trans":    outsam.write(line)
+                else:                   outsam.write('\t'.join(align[0:19])+'\n')
 
-if __name__ == "__main__":
-    main()
-    
+sam1.close()
+sam2.close()
+outsam.close()
