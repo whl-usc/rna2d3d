b6594277c0b8656ecdb16fd2ddef960a1702685f
Wilson <wlee9829@gmail.com>
2024-02-16 14:44:44 -0800

    Updated mapping_v2.sh
    Addressed issue with SLURM scripting for memory for tasks.

--- shell/mapping_v2.sh
+++ shell/mapping_v2.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #SBATCH --ntasks=1
 #SBATCH --time=12:00:00
-#SBATCH --mem-per-cpu=32G
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=8G
 
 module load gcc/11.3.0
 module load openjdk
