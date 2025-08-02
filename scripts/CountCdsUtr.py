f4cdcb2fc2771eec55e904dc92ab65ed83840885
Wilson <wlee9829@gmail.com>
2025-08-01 19:47:18 -0700

    Update CountCdsUtr.py
    Complete overhaul of the CountCdsUtr script. Re-wrote for debugging and clearer error handling.

--- scripts/CountCdsUtr.py
+++ scripts/CountCdsUtr.py
@@ -1,228 +1,307 @@
+#!/usr/bin/env python3
+
 """
-minjiezhang123@gmail.com    2020-01-16
-Used to count the reads number of CDS, 5UTR and 3UTR region.
-eg: python CountCdsUtr.py test.bam hg38mask14add_count.bed test
-
-Only the longest transcript was used for annotation.
+Contact:    wlee9829@gmail.com
+Date:       2025_07_31
+Python:     python3.10
+Script:     CountCdsUtr.py
 """
-
-
-#1. input and output setup
 ################################################################################
+# Define version
+__version__ = "1.2.0"
+
+# Version notes
+__update_notes__ = """
+1.2.0
+    -   Adjust biotypes to be by unique read counts.
+1.1.0
+    -   Rework counting logic to be stringent per base.
+    -   Fix RPKM/RPM calculations to use all read counts.
+1.0.0
+    -   Refactor code from the original CountCdsUtr.py script.
+"""
 ################################################################################
-#this section sets up the input and output
-import sys, numpy, os, re, itertools, random
+
+# Import Packages
+from collections import defaultdict
 from datetime import datetime
-from multiprocessing import Process, Lock, Manager
-from collections import Counter
+import argparse
+import itertools
+import os
+import subprocess
 
-if len(sys.argv) < 3:
-    print("Usage:       python  CountCdsUtr.py inputbam  CdsUtr.bed  exclude_type  outputprefix")
-    print("inputbam:    Bam file")
-    print("CdsUtr.bed   Bed file of CDS and 5UTR, 3UTR regions")
-    print("             eg: hg38mask14add_count.bed")
-    print("exclude_type:eg: snoRNA,rRNA")
-    print("outputprefix Outputprefix")
-    print("")
-    print("eg:  python CountCdsUtr.py   test.bam   hg38mask14add_count.bed   test")
-    sys.exit()
-
-inputbam = sys.argv[1]
-annobed = sys.argv[2]
-exclude_types = sys.argv[3].split(',')
-outputprefix = sys.argv[4]
-genes = {}; genesdict = {}; cdsdict = {}; intronsdict = {}; fiveUtrdict = {}; threeUtrdict = {}; biotypedict = {};  inputcount = 0 
 ################################################################################
 
-
-#2. Processing bam file
-##############################################################################
-########################## Process bam to bed file ###########################
-print(str(datetime.now())[:-7], "Process bam to bed file ...")
-outputbed = outputprefix+'.bed'
-os.system("/project/zhipengl_72/minjiez/software/bedtools2/bin/bedtools bamtobed -split -i %s > %s" % (inputbam, outputbed))
-os.system("awk '{if($0!~/^chr/){print \"chr\"$0} else {print $0}}' %s > %s" % (outputbed, 'temp.bed'))
-os.system("sort -k1,1 -k2,2n -o %s %s" % (outputbed, 'temp.bed'))
-#totalreads = len(open(outputbed,'rU').readlines())
+# Define all sub-functions for processing
+def get_time():
+    return str(datetime.now())[:-7]
 
 
-## get total reads number
-f= open(outputbed,'r')
-readsdict = {}
-for line in f:
-    readID = line.split('\t')[3]
-    readsdict[readID] = 1
-f.close()
-totalreads = len(readsdict)
-
-
-######################## get length of CDS and UTR ###########################
-print(str(datetime.now())[:-7], "Extract CDS/UTR length ...")
-annofile = open(annobed,'r')
-genelendict = {};   cdslendict = {};    intronlendict = {};    fiveUtrlendict = {};    threeUtrlendict = {}
-for line in annofile:
-    align = line.split('\t')
-    length = int(align[2]) - int(align[1]) + 1
-    genes[align[4]] = align[6].rstrip('\n')
-    if align[4] not in genelendict: genelendict[align[4]] = 0
-    if align[4] not in intronlendict: intronlendict[align[4]] = 0
-    if align[4] not in cdslendict: cdslendict[align[4]] = 0
-    if align[4] not in fiveUtrlendict: fiveUtrlendict[align[4]] = 0
-    if align[4] not in threeUtrlendict: threeUtrlendict[align[4]] = 0
-    genelendict[align[4]] += int(length)
-    if align[5] == "CDS":   cdslendict[align[4]] += int(length)
-    if align[5] == "Intron":   intronlendict[align[4]] += int(length)
-    if align[5] == "five_utr":   fiveUtrlendict[align[4]] += int(length)
-    if align[5] == "three_utr":   threeUtrlendict[align[4]] += int(length)
-annofile.close()
-
-
-
-##########################   Get overlap  ###########################
-print(str(datetime.now())[:-7], "Get overlap between bed and anno file ...")
-outputoverlap = outputprefix+'_overlap.bed'
-os.system("/project/zhipengl_72/minjiez/software/bedtools2/bin/bedtools intersect -a %s -b %s -F 0.5 -wa -wb > %s" % (annobed, outputbed, outputoverlap))
-
-
-
-#chr1    3216025 3216968 -       Xkr4    CDS     protein_coding  \
-#chr1    3216327 3216348 A00208:91:HHKVTDRXX:1:2237:27543:4131   50      -
-########################## Counting CDS, 5UTR, 3UTR reads ###########################
-#readsdict = {}
-print(str(datetime.now())[:-7], "Counting CDS, 5UTR, 3UTR reads ...")
-inputbed = open(outputoverlap,'r')
-for line in inputbed:
-    align = line.split()
-    gene, region, readID, biotype = align[4], align[5], align[10], align[6]
-    inputcount += 1
-    if not inputcount%1000000:
-        print(str(datetime.now())[:-7], "Processed", inputcount, "reads ...")
+# 1. Process the BAM file
+def bam2bed(input_bam, output_bed):
+    print(f"{get_time()}\tConverting BAM to BED file using bedtools...")
     
-    if biotype not in exclude_types:
-        readsdict[readID] = 1
-        if biotype not in biotypedict:  biotypedict[biotype]={}
-        if gene not in genesdict: genesdict[gene]=0
-        if gene not in cdsdict: cdsdict[gene]=0
-        if gene not in intronsdict: intronsdict[gene]=0
-        if gene not in fiveUtrdict: fiveUtrdict[gene]=0
-        if gene not in threeUtrdict: threeUtrdict[gene]=0
+    temp_unsorted = output_bed + ".unsorted"
     
-        biotypedict[biotype][readID] = 1
-        genesdict[gene] += 1
-        if region == "CDS":
-            cdsdict[gene]+=1
-        if region == "Intron":
-            intronsdict[gene]+=1
-        if region == "five_utr":
-            fiveUtrdict[gene]+=1
-        if region == "three_utr":
-            threeUtrdict[gene]+=1
-inputbed.close()
+    with open(temp_unsorted, "w") as out:
+        subprocess.run(["bedtools", "bamtobed", "-i", input_bam, "-split"], stdout=out, check=True)
 
-#totalreads = len(readsdict)
+    with open(output_bed, "w") as sorted_out:
+        subprocess.run(["sort", "-k1,1", "-k2,2n", temp_unsorted], stdout=sorted_out, check=True)
 
-output = open(outputprefix+'_RPKM.txt','w')
-outstring="Gene" + '\t' + "Biotype" + '\t' + "gene_RPKM" + '\t' + "CDS_RPKM" + '\t' + "Intron_RPKM" + '\t' + "5UTR_RPKM" + '\t' + "3UTR_RPKM" + '\n'
-output.write(outstring)
-for i in sorted(genes):
-    if i not in genesdict or str(genesdict[i]) == "0": generpkm = "0.01";
-    else:
-        generpkm = genesdict[i]/(genelendict[i]/1000 * int(totalreads)/1000000)
-    
-    if i not in cdsdict or str(cdsdict[i]) == "0": cdsrpkm = "0.01";
-    else:
-        cdsrpkm = cdsdict[i]/(cdslendict[i]/1000 * int(totalreads)/1000000)
-    
-    if i not in intronsdict or str(intronsdict[i]) == "0": intronrpkm = "0.01";
-    else:
-        intronrpkm = intronsdict[i]/(intronlendict[i]/1000 * int(totalreads)/1000000)
-        
-    if i not in fiveUtrdict or str(fiveUtrdict[i]) == "0":  fiveUtrrpkm = "0.01";
-    else:
-        fiveUtrrpkm = fiveUtrdict[i]/(fiveUtrlendict[i]/1000 * int(totalreads)/1000000)
-    
-    if i not in threeUtrdict or str(threeUtrdict[i]) == "0":    threeUtrrpkm = "0.01";
-    else:
-        threeUtrrpkm = threeUtrdict[i]/(threeUtrlendict[i]/1000 * int(totalreads)/1000000)
-    
-    outstring = i + '\t' +  genes[i]+ '\t' + str(generpkm) + '\t' + str(cdsrpkm) + '\t' + str(intronrpkm) + '\t' + str(fiveUtrrpkm) + '\t' + str(threeUtrrpkm) + '\n'
-    output.write(outstring)
-output.close()
+    read_ids = set()
+    with open(output_bed) as f:
+        for line in f:
+            fields = line.strip().split('\t')
+            if len(fields) >= 4:
+                read_ids.add(fields[3])
+    os.remove(temp_unsorted)
+
+    return len(read_ids)
 
 
+# 2. Process the annotation file
+def read_anno(annotation_bed):
+    genelendict = defaultdict(int)
+    cdslendict = defaultdict(int)
+    intronlendict = defaultdict(int)
+    fiveUtrlendict = defaultdict(int)
+    threeUtrlendict = defaultdict(int)
+    genes = {}
+    biotypes = {}
 
-output = open(outputprefix+'_RPM.txt','w')
-outstring="Gene" + '\t' + "Biotype" + '\t' + "gene_RPM" + '\t' + "CDS_RPM" + '\t' + "Intron_RPM" + '\t' + "5UTR_RPM" + '\t' + "3UTR_RPM" + '\n'
-output.write(outstring)
+    with open(annotation_bed, 'r') as f:
+        for line in f:
+            fields = line.strip().split('\t')
+            chrom, start, end = fields[0], int(fields[1]), int(fields[2])
+            strand = fields[3]
+            gene_id = fields[4]
+            region_type = fields[5].lower() 
+            biotype = fields[6]
+            
+            length = end - start + 1
+            genes[gene_id] = strand
+            biotypes[gene_id] = biotype
+            genelendict[gene_id] += length
 
-print('total reads:\t'+str(totalreads)+'\n')
+            if region_type == "cds":
+                cdslendict[gene_id] += length
+            elif region_type == "intron":
+                intronlendict[gene_id] += length
+            elif region_type == "five_utr":
+                fiveUtrlendict[gene_id] += length
+            elif region_type == "three_utr":
+                threeUtrlendict[gene_id] += length
 
-for i in sorted(genes):
-    if i not in genesdict or str(genesdict[i]) == "0": generpkm = "0.01";
-    else:
-        generpkm = genesdict[i]/int(totalreads)*1000000
-    
-    if i not in cdsdict or str(cdsdict[i]) == "0": cdsrpkm = "0.01";
-    else:
-        cdsrpkm = cdsdict[i]/int(totalreads)*1000000
-    
-    if i not in intronsdict or str(intronsdict[i]) == "0": intronrpkm = "0.01";
-    else:
-        intronrpkm = intronsdict[i]/int(totalreads)*1000000
-        
-    if i not in fiveUtrdict or str(fiveUtrdict[i]) == "0":  fiveUtrrpkm = "0.01";
-    else:
-        fiveUtrrpkm = fiveUtrdict[i]/int(totalreads)*1000000
-    
-    if i not in threeUtrdict or str(threeUtrdict[i]) == "0":    threeUtrrpkm = "0.01";
-    else:
-        threeUtrrpkm = threeUtrdict[i]/int(totalreads)*1000000
-    
-    outstring = i + '\t' +  genes[i]+ '\t' + str(generpkm) + '\t' + str(cdsrpkm) + '\t' + str(intronrpkm) + '\t' + str(fiveUtrrpkm) + '\t' + str(threeUtrrpkm) + '\n'
-    output.write(outstring)
-output.close()
+    return {
+        'genes': genes,
+        'biotypes': biotypes,
+        'gene_lengths': genelendict,
+        'cds_lengths': cdslendict,
+        'intron_lengths': intronlendict,
+        'five_utr_lengths': fiveUtrlendict,
+        'three_utr_lengths': threeUtrlendict
+    }
 
 
+def get_overlap(annotation_bed, bed_file, outputprefix):
+    output_overlap = f"{outputprefix}_overlap.bed"
+    cmd = [
+        "bedtools", "intersect",
+        "-a", annotation_bed,
+        "-b", bed_file,
+        "-F", "0.5",
+        "-wa", "-wb",
+        "-sorted"
+    ]
+    with open(output_overlap, "w") as fout:
+        subprocess.run(cmd, stdout=fout, check=True)
+    return output_overlap
 
-output = open(outputprefix+'_ReadCount.txt','w')
-outstring="Gene" + '\t' + "Biotype" + '\t' + "gene_Reads" + '\t' + "CDS_Reads" + '\t' + "Intron_Reads" + '\t' + "5UTR_Reads" + '\t' + "3UTR_Reads" + '\n'
-output.write(outstring)
-for i in sorted(genes):
-    if i not in genesdict or str(genesdict[i]) == "0": generpkm = "0.01";
-    else:
-        generpkm = genesdict[i]
-    
-    if i not in cdsdict or str(cdsdict[i]) == "0": cdsrpkm = "0.01";
-    else:
-        cdsrpkm = cdsdict[i]
-    
-    if i not in intronsdict or str(intronsdict[i]) == "0": intronrpkm = "0.01";
-    else:
-        intronrpkm = intronsdict[i]
-        
-    if i not in fiveUtrdict or str(fiveUtrdict[i]) == "0":  fiveUtrrpkm = "0.01";
-    else:
-        fiveUtrrpkm = fiveUtrdict[i]
-    
-    if i not in threeUtrdict or str(threeUtrdict[i]) == "0":    threeUtrrpkm = "0.01";
-    else:
-        threeUtrrpkm = threeUtrdict[i]
-    
-    outstring = i + '\t' +  genes[i]+ '\t' + str(generpkm) + '\t' + str(cdsrpkm) + '\t' + str(intronrpkm) + '\t' + str(fiveUtrrpkm) + '\t' + str(threeUtrrpkm) + '\n'
-    output.write(outstring)
-output.close()
 
-"""    
-totalreads = 0    
-for i in biotypedict:
-    totalreads += len(biotypedict[i])
-"""
+def count_reads(overlap_file, exclude_types, gene_biotypes):
+    counts = {
+        'cds': defaultdict(int),
+        'introns': defaultdict(int),
+        'five_utr': defaultdict(int),
+        'three_utr': defaultdict(int),
+        'biotypes': {},
+        'total_reads': 0,
+        'gene_reads': defaultdict(int),
+        'gene_read_sets': defaultdict(set)
+    }
+
+    exclude_set = set(exclude_types)
+
+    with open(overlap_file) as f:
+        for line in f:
+            try:
+                fields = line.strip().split('\t', 11)
+                gene_id = fields[4]
+                region_type = fields[5].lower()
+                bio_type = fields[6]
+                read_id = fields[10]
+            except IndexError:
+                continue
+
+            if bio_type in exclude_types:
+                continue
+
+            counts['gene_reads'][gene_id] += 1
+            counts['gene_read_sets'][gene_id].add(read_id)
+            counts['biotypes'][gene_id] = bio_type
+            counts['total_reads'] += 1
+
+            if region_type == "cds":
+                counts['cds'][gene_id] += 1
+            elif region_type == "intron":
+                counts['introns'][gene_id] += 1
+            elif region_type == "five_utr":
+                counts['five_utr'][gene_id] += 1
+            elif region_type == "three_utr":
+                counts['three_utr'][gene_id] += 1
     
-output = open(outputprefix+'_biotype.txt','w')
-for i in sorted (biotypedict):
-    outstring = i + '\t' + str(len(biotypedict[i])) + '\t' + str(len(biotypedict[i])/totalreads) + '\n'
-    output.write(outstring)
-output.close()
- 
- 
-os.system("rm %s %s %s" % (outputbed, 'temp.bed', outputoverlap))
+    counts['gene_counts'] = dict(counts['gene_reads'])
+    counts['gene_read_sets'] = counts['gene_read_sets']
+    
+    return counts
+    
+
+def write_output(prefix, genes, biotypes, counts, total_reads, *dicts):
+    rpkm_file = f"{prefix}_RPKM.txt"
+    rpm_file = f"{prefix}_RPM.txt"
+    count_file = f"{prefix}_ReadCount.txt"
+    biotype_file = f"{prefix}_biotype.txt"
+
+    genelen, cdslen, intronlen, fiveutrlen, threeutrlen = dicts[:5]
+    gene_counts, cdsdict, introndict, fiveutrdict, threeutrdict = dicts[5:10]
+
+    def calc_rpkm(count, length):
+        return 0.01 if count == 0 or length == 0 else count / (length / 1000 * total_reads / 1_000_000)
+
+    def calc_rpm(count):
+        return 0.01 if count == 0 else count / total_reads * 1_000_000
+
+    with open(rpkm_file, "w") as f:
+        f.write("Gene\tBiotype\tgene_RPKM\tCDS_RPKM\tIntron_RPKM\t5UTR_RPKM\t3UTR_RPKM\n")
+        for gene in sorted(genes):
+            biotype = biotypes.get(gene, "NA")
+            line = f"{gene}\t{biotype}\t" + "\t".join([
+                f"{calc_rpkm(gene_counts.get(gene, 0), genelen.get(gene, 0)):.2f}",
+                f"{calc_rpkm(cdsdict.get(gene, 0), cdslen.get(gene, 0)):.2f}",
+                f"{calc_rpkm(introndict.get(gene, 0), intronlen.get(gene, 0)):.2f}",
+                f"{calc_rpkm(fiveutrdict.get(gene, 0), fiveutrlen.get(gene, 0)):.2f}",
+                f"{calc_rpkm(threeutrdict.get(gene, 0), threeutrlen.get(gene, 0)):.2f}"
+            ]) + "\n"
+            f.write(line)
+
+    with open(rpm_file, "w") as f:
+        f.write("Gene\tBiotype\tgene_RPM\tCDS_RPM\tIntron_RPM\t5UTR_RPM\t3UTR_RPM\n")
+        for gene in sorted(genes):
+            biotype = biotypes.get(gene, "NA")
+            line = f"{gene}\t{biotype}\t" + "\t".join([
+                f"{calc_rpm(gene_counts.get(gene, 0)):.2f}",
+                f"{calc_rpm(cdsdict.get(gene, 0)):.2f}",
+                f"{calc_rpm(introndict.get(gene, 0)):.2f}",
+                f"{calc_rpm(fiveutrdict.get(gene, 0)):.2f}",
+                f"{calc_rpm(threeutrdict.get(gene, 0)):.2f}"
+            ]) + "\n"
+            f.write(line)
+
+    with open(count_file, "w") as f:
+        f.write("Gene\tBiotype\tgene_Reads\tCDS_overlaps\tIntron_overlaps\t5UTR_overlaps\t3UTR_overlaps\n")
+        for gene in sorted(genes):
+            biotype = biotypes.get(gene, "NA")
+            line = f"{gene}\t{biotype}\t" + "\t".join([
+                str(gene_counts.get(gene, 0)),
+                str(cdsdict.get(gene, 0)),
+                str(introndict.get(gene, 0)),
+                str(fiveutrdict.get(gene, 0)),
+                str(threeutrdict.get(gene, 0))
+            ]) + "\n"
+            f.write(line)
+
+    with open(biotype_file, "w") as f:  
+        f.write("Biotype\tgene_Reads\tFraction\tPercentage\n")
+
+        read_biotype_map = defaultdict(set)
+        for gene_id, reads in counts['gene_read_sets'].items():
+            biotype = biotypes.get(gene_id, "unknown")
+            for read_id in reads:
+                read_biotype_map[read_id].add(biotype)
+
+        biotype_counts = defaultdict(int)
+        for biotypes in read_biotype_map.values():
+            for biotype in biotypes:
+                biotype_counts[biotype] += 1
+                
+        for biotype in sorted(biotype_counts):
+            count = biotype_counts[biotype]
+            fraction = count / total_reads if total_reads > 0 else 0
+            f.write(f"{biotype}\t{count}\t{fraction:.6f}\t{fraction*100:.2f}%\n")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Counts reads in CDS, 5'UTR, and 3'UTR from BAM using BED.")
+    parser.add_argument("bam", help="Input BAM file (sorted and indexed)")
+    parser.add_argument("bed", help=(
+                        "Annotation BED file with regions labeled" 
+                        "in column 4 (e.g., CDS, 5UTR, 3UTR)"))
+    VALID_BIOTYPES = {
+        "none", "lncRNA", "miRNA", "misc_RNA", "mt-tRNA", "protein_coding",
+        "RNR", "rRNA", "scaRNA", "scRNA", "SNORA", "SNORD", "snRNA", "tRNA"
+        }
+    parser.add_argument(
+        "exclude",
+        type=lambda s: [x.strip() for x in s.split(",")],
+        default=[],
+        help="Exclude comma-separated bioType(s) (e.g., --exclude=miRNA,tRNA)")
+    parser.add_argument("outprefix", help="Output file prefix")
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    bam_file = args.bam
+    annotation_bed = args.bed
+    exclude = set(args.exclude)
+    outprefix = args.outprefix
+    
+    bed_file = outprefix + ".bed"
+    number_reads = bam2bed(bam_file, bed_file)
+    print(f"\t\t\t{number_reads} alignments processed.")
+    anno_data = read_anno(annotation_bed)
+
+    print(f"{get_time()}\tChecking for overlaps...")
+    overlap_file = get_overlap(annotation_bed, bed_file, outprefix)
+    counts = count_reads(overlap_file, exclude, anno_data['biotypes'])
+    gene_counts = counts['gene_counts']
+
+    write_output(
+        outprefix, 
+        set(gene_counts.keys()), 
+        counts['biotypes'],
+        counts,
+        number_reads,
+        anno_data['gene_lengths'],
+        anno_data['cds_lengths'],
+        anno_data['intron_lengths'],
+        anno_data['five_utr_lengths'],
+        anno_data['three_utr_lengths'],
+        gene_counts,
+        counts['cds'],
+        counts['introns'],
+        counts['five_utr'],
+        counts['three_utr']
+    )
+
+    for temp_file in [bed_file, overlap_file]:
+        if os.path.exists(temp_file):
+            os.remove(temp_file)
+
+    print(f"{get_time()}\tComplete. See files with prefix '{outprefix}'...")
+
+if __name__ == "__main__":
+    main()
